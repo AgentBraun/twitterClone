85e9898f2f3d5535d8c20e88c22dc79696694ef2
AgentBraun <AgentBraun@users.noreply.github.com>
2023-06-08 11:14:51 +0200

    tweet crud operation almost done

--- backend/src/routes/tweets.ts
+++ backend/src/routes/tweets.ts
@@ -1,30 +1,48 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 // all tweets
-router.get('/', (req, res) => {
-  res.status(501).json({ error: 'not implemented Get all tweets' });
+router.get('/', async (req, res) => {
+  const allTweets = await prisma.tweet.findMany();
+  res.json(allTweets);
 });
 
-// get one user
-router.get('/:tweetID', (req, res) => {
+// get one tweet
+router.get('/:tweetID', async (req, res) => {
   const { tweetID } = req.params;
-  res.status(501).json({ error: `not implemented via get tweet with tweetID: ${tweetID}` });
+  const tweet = await prisma.tweet.findUnique({ where: { id: Number(tweetID) } });
+  res.json(tweet);
 });
 
-router.post('/tweet', (req, res) => {
-  res.status(501).json({ error: 'not implemented create a tweet' });
+//create a tweet
+router.post('/tweet', async (req, res) => {
+  const { content, image, userID } = req.body;
+  try {
+    const tweet = await prisma.tweet.create({
+      data: {
+        content,
+        image,
+        impressions: 0,
+        userID,
+      },
+    });
+    res.json(tweet);
+  } catch (error) {
+    res.status(400).json({ error: 'something went wrong' });
+  }
 });
 
 // update user
-router.put('/:tweetID', (req, res) => {
+router.put('/:tweetID', async (req, res) => {
   const { tweetID } = req.params;
   res.status(501).json({ error: `not implemented via uptdate tweet with tweetID: ${tweetID}` });
 });
 
 // delete user
-router.delete('/:tweetID', (req, res) => {
+router.delete('/:tweetID', async (req, res) => {
   const { tweetID } = req.params;
   res.status(501).json({ error: `not implemented via delete tweet with tweetID: ${tweetID}` });
 });
